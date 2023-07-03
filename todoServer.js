2710cc5ea4ce4058fc72a77849bab6054f281f7e
Abhijit Sahoo <abhijitsahooas.01@gmail.com>
2023-07-03 13:29:21 +0530

    Update todoServer.js
    updated path file

--- todoServer.js
+++ todoServer.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
+const path = require("node:path");
 const port = 3000;
 
 const app = express();
